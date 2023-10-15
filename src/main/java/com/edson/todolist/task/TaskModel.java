c2b95ad3d2ece6a2b23689bfc3becfa13baea8cc
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-10-15 12:37:49 -0300

    criand a task

---
+++ src/main/java/com/edson/todolist/task/TaskModel.java
@@ -0,0 +1,37 @@
+package com.edson.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_task")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String description;
+    @Column(length = 50)
+    private String title;
+
+    @CreationTimestamp
+    private LocalDateTime startAt;
+
+    @CreationTimestamp
+    private LocalDateTime endAt;
+
+    private String priority;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    private UUID idUser;
+}
--- src/main/java/com/edson/todolist/user/UserController.java
+++ src/main/java/com/edson/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -17,12 +19,18 @@ public class UserController {
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel) {
+
+       
         var user = this.userRepository.findByUsername(userModel.getUsername());
         if (user != null) {
             System.out.println("Usuario ja existe");
             // mensagem de erro
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
         }
+
+        var passwordHashred= BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred);
+
         var useCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(useCreated);
     }
